f3d45f17f588108aa482405a44a4dd178c80c4bd
Chalisebinod <chalisebinod40@gmail.com>
2025-04-10 18:22:54 +0545

    Clean slate: removed all files

--- backend/route/paymentRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { checkAuthentication } = require("../middleware/middleware");
-const { initiatePayment, verifyPayment } = require("../controller/paymentController");
-const router = express.Router();
-router.post("/initiate", checkAuthentication, initiatePayment);
-router.post("/verify", checkAuthentication, verifyPayment);
-
-module.exports = router;
