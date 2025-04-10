9e050495b19fa6b0211624413c7d24af78dab555
Chalisebinod <chalisebinod40@gmail.com>
2025-04-10 18:22:54 +0545

    Clean slate: removed all files

--- backend/route/foodRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const foodController = require("../controller/foodController");
-const { checkAuthentication } = require("../middleware/middleware");
-
-router.post("/add", checkAuthentication ,foodController.addFood); // Add food to a venue
-router.get("/venue/:venueId",checkAuthentication , foodController.getFoodsByVenue); // Get all foods for a venue
-
-module.exports = router;
